02c0d13812821aa126915782368810142fa52dba
nicoman35 <christian_nicolau@yahoo.de>
2021-08-23 12:42:17 +0200

    minor typo

--- mod.cpp
+++ mod.cpp
@@ -7,6 +7,6 @@ logo			= "logo.paa";
 logoOver		= "logo.paa";
 tooltip			= "UCAV Sentinel Enhanced v1.2";
 tooltipOwned	= "NIC UCAV";
-overview		= "This enhanced UCAV Sentinel has several enhancements:<br />- targeting pod has a magnification up to 80x, and raised move freedom<br />- increased sensor range<br />- increased ACE Maveric missile and GBU SDB ranges<br />- 'AI refresh' action to prevent AI from doing unwanted things"; 
+overview		= "The enhanced UCAV Sentinel has several enhancements:<br />- targeting pod has a magnification up to 80x, and raised move freedom<br />- increased sensor range<br />- increased ACE Maveric missile and GBU SDB ranges<br />- 'AI refresh' action to prevent AI from doing unwanted things"; 
 author			= "NIC";
 overviewPicture	= "logo.paa";
